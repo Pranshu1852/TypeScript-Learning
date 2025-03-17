483ad83ebbe43d94ff8806bc771e216db0249f89
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-03-17 20:09:52 +0530

    Add(Mini Project): Created dynamically generate todo list

--- Learning/Mini_Project/src/index.ts
+++ Learning/Mini_Project/src/index.ts
@@ -1,10 +1,18 @@
 const btn=document.getElementById('btn')! as HTMLButtonElement;
 const input=document.getElementById('todoinput')! as HTMLInputElement;
 const form=document.querySelector('form')!;
+const list=document.getElementById('todolist')!;
 
 function handleSubmit(e:Event){
     e.preventDefault();
-    console.log("Submitted!");
+    const newTodo=input.value;
+    const newLI=document.createElement('li');
+    const checkbox=document.createElement('input');
+    checkbox.type="checkbox";
+    newLI.append(newTodo);
+    newLI.append(checkbox);
+    list.append(newLI);
+    input.value="";
 }
 
 form.addEventListener("submit",handleSubmit);
