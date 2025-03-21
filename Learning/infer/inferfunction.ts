aa6f4bfe48218d71b83fbf46c6d99d7b766623a4
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-03-21 19:54:21 +0530

    Add(infer): Learn about infer type in function return type

--- Learning/infer/inferfunction.ts
+++ Learning/infer/inferfunction.ts
@@ -4,3 +4,14 @@ type FirstParam<T>=T extends (name:infer U,...args: any[])=> any ? U:never;
 type Test4 = FirstParam<(x: string, y: number) => void>;
 type Test5 = FirstParam<(flag: boolean) => number>; 
 type Test6 = FirstParam<() => void>;
+
+
+
+
+
+
+type ReturnTypeOf<T>=T extends (...args: any[])=>infer U ? U:never;
+
+type Test9 = ReturnTypeOf<() => number>;
+type Test10 = ReturnTypeOf<() => Promise<string>>;
+type Test11 = ReturnTypeOf<(x: number) => boolean>;
