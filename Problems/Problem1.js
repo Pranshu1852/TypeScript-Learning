e271b949413c849bf686cbca79ac815ae6c4f861
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-03-24 11:13:31 +0530

    Add(Problems): Problem1 about extract keys from object in type string

---
+++ Problems/Problem1.js
@@ -0,0 +1,8 @@
+"use strict";
+//1) Extract all keys of an object as an array of strings?
+const user = { name: "John", age: 30 }; // Expected Result:["name", "age"]// When you get the result, try to check it's type, it shouldn't be the type of `string[]`. Instead it should be ("name" | "age")[].
+let arrKey = [];
+for (let key in user) {
+    arrKey.push(key);
+}
+console.log(arrKey);
