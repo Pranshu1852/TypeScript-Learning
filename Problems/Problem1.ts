152bda71eef7c8674cea9cc5634b13bf83049da6
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-03-26 19:21:19 +0530

    Fix(Problems): fix the issue in problem1

--- Problems/Problem1.ts
+++ Problems/Problem1.ts
@@ -1,10 +1,14 @@
 //1) Extract all keys of an object as an array of strings?
 const user = { name: "John", age: 30 };// Expected Result:["name", "age"]// When you get the result, try to check it's type, it shouldn't be the type of `string[]`. Instead it should be ("name" | "age")[].
 
-let arrKey: string[]=[];
+type keyo<T>=T extends Record<infer U,any>? U[]:T;
+type keytype=keyo<{ name: "John", age: 30 }>;
 
-for(let key in user){
-    arrKey.push(key);
+let arr:keytype=[];
+for(let a in user){
+    if(Object.keys(user).includes(a)){
+        arr.push(a as keyof typeof user);
+    }
 }
 
-console.log(arrKey);
+console.log(arr);
